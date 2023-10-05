3a83bdb2944b5b4f6ace88c6f7a2dc42b0a2a5f3
Andy Tran <AyyyTran@gmail.com>
2023-10-04 21:03:22 -0700

    controllers complete added image to schema

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,7 +1,28 @@
 const Blog = require('../models/BlogModel.js');
-
+const mongoose = require('mongoose');
 //GET all blogs
+const getAllBlogs = async (req, res) => {
+  const blogs = await Blog.find({}).sort({createdAt: -1});
+  res.status(200).json(blogs);
+};
+
 //GET a certain blog
+const getBlog = async (req, res) => {
+  const {id} = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such blog exists'});
+  }
+
+  const blog = await Blog.findById(id);
+
+  if (!blog) {
+    return res.status(404).json({error: 'No such blog exists'});
+  }
+
+  res.status(200).json(blog);
+};
+
 // POST a new blog
 const createBlog = async (req, res) => {
   const {title, author, content} = req.body;
@@ -14,9 +35,43 @@ const createBlog = async (req, res) => {
     res.status(400).json({error: error.message});
   }
 };
+
 // DELETE a blog
+const deleteBlog = async (req, res) => {
+  const {id} = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such blog exists'});
+  }
+
+  const blog = await Blog.findOneAndDelete({_id: id});
+
+  if (!blog) {
+    return res.status(404).json({error: 'No such blog exists'});
+  }
+
+  res.status(200).json(blog);
+};
+
 // UPDATE a blog
+const updateBlog = async (req, res) => {
+  const {id} = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such blog exists'});
+  }
+
+  const blog = await Blog.findOneAndUpdate({_id: id}, {...req.body});
+
+  if (!blog) {
+    return res.status(404).json({error: 'No such blog exists'});
+  }
+
+  res.status(200).json(blog);
+};
 
 module.exports = {
   createBlog,
+  getAllBlogs,
+  getBlog,
+  deleteBlog,
+  updateBlog,
 };
--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -1,28 +1,26 @@
 const express = require('express');
-const {createBlog} = require('../controllers/blogController');
+const {
+  createBlog,
+  getAllBlogs,
+  getBlog,
+  deleteBlog,
+  updateBlog,
+} = require('../controllers/blogController');
 
 const router = express.Router();
 
 //GET all blogs
-router.get('/', (req, res) => {
-  res.json({msg: 'GET all blogs'});
-});
+router.get('/', getAllBlogs);
 
 //GET a certain blog
-router.get('/:id', (req, res) => {
-  res.json({msg: 'GET a blog'});
-});
+router.get('/:id', getBlog);
 
 // POST a new blog
 router.post('/', createBlog);
 
 // DELETE a blog
-router.delete('/:id', (req, res) => {
-  res.json({msg: 'DELETE a blog'});
-});
+router.delete('/:id', deleteBlog);
 // UPDATE a blog
-router.patch('/:id', (req, res) => {
-  res.json({msg: 'UPDATE a blog'});
-});
+router.patch('/:id', updateBlog);
 
 module.exports = router;
