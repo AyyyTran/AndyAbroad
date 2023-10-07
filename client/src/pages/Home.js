2d0ff803e5a6e97f39cf2b38ed04683abc856f3f
Andy Tran <AyyyTran@gmail.com>
2023-10-06 22:39:19 -0700

    created react app and home page and navbar comp

---
+++ client/src/pages/Home.js
@@ -0,0 +1,28 @@
+import {useEffect, useState} from 'react';
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const response = await fetch('/api/blogs');
+      const json = await response.json();
+
+      if (response.ok) {
+        setBlogs(json);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="blogs">
+        {blogs && blogs.map((blog) => <p key={blog._id}>{blog.title}</p>)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
