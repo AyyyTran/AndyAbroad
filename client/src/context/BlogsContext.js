1591f411bcbf9640f96e4c7ac4425d3c12a63bed
Andy Tran <AyyyTran@gmail.com>
2023-11-01 22:25:14 -0700

    created useBlogsContext and a hook to call context

---
+++ client/src/context/BlogsContext.js
@@ -0,0 +1,30 @@
+import {createContext, useReducer} from 'react';
+
+export const BlogsContext = createContext();
+
+export const blogsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_BLOGS':
+      return {
+        blogs: action.payload,
+      };
+    case 'CREATE_BLOG':
+      return {
+        blogs: [action.payload, ...state.blogs],
+      };
+    default:
+      return state;
+  }
+};
+
+export const BlogsContextProvider = ({children}) => {
+  // this is a hook with reducer and empty onject
+  const [state, dispatch] = useReducer(blogsReducer, {blogs: null});
+
+  dispatch({});
+  return (
+    <BlogsContext.Provider value={{state, dispatch}}>
+      {children}
+    </BlogsContext.Provider>
+  );
+};
