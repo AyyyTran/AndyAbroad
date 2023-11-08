7ab52e261783f089a0cdec442a3fca6877cddafc
Andy Tran <AyyyTran@gmail.com>
2023-11-07 22:10:18 -0800

    created dispatch hook for delete blog

--- client/src/components/BlogDetails.js
+++ client/src/components/BlogDetails.js
@@ -1,9 +1,27 @@
-const BlogDetails = ({blog}) => (
-  <div className="blog-details">
-    <img className="blogImg" src={`/images/${blog.coverImage}`} alt="blog" />
-    <h4>{blog.title}</h4>
-    <p>{blog.content}</p>
-  </div>
-);
+import {useBlogsContext} from '../hooks/useBlogsContext';
+
+const BlogDetails = ({blog}) => {
+  const {dispatch} = useBlogsContext();
+
+  const handleClick = async () => {
+    const response = await fetch('./api/blogs/' + blog._id, {
+      method: 'DELETE',
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_BLOG', payload: json});
+    }
+  };
+
+  return (
+    <div className="blog-details">
+      <img className="blogImg" src={`/images/${blog.coverImage}`} alt="blog" />
+      <h4>{blog.title}</h4>
+      <p>{blog.content}</p>
+      <span onClick={handleClick}>Delete</span>
+    </div>
+  );
+};
 
 export default BlogDetails;
